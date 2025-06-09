cc2e2ddc3fc90447d6a0529a21fad067e7c87367
Asim <asim1306iiit@gmail.com>
2025-06-09 19:23:16 +0530

    Create trie.cpp

---
+++ trie.cpp
@@ -0,0 +1,72 @@
+struct Node {
+    Node* links[26];
+    bool flag = false;
+
+    bool containsKey(char ch) {
+        return links[ch - 'a'] != NULL;
+    }
+
+    Node* get(char ch) {
+        return links[ch - 'a'];
+    }
+
+    void put(char ch, Node* node) {
+        links[ch - 'a'] = node;
+    }
+
+    void setEnd() {
+        flag = true;
+    }
+
+    bool isEnd() {
+        return flag;
+    }
+};
+
+class Trie{
+    private:
+    Node* root;
+
+    public:
+    Trie() {
+        root = new Node();
+    }
+
+    // insert a word, TC=O(len)
+    void insert(string word) {
+        Node* node = root;
+        for (char ch : word) {
+            if (!node->containsKey(ch)) {
+                node->put(ch, new Node());
+            }
+            node = node->get(ch);
+        }
+        node->setEnd();
+    }
+
+    // search for a word, in trie
+    bool search(string word) {
+        Node* node = root;
+        for (char ch : word) {
+            if (!node->containsKey(ch)) {
+                return false;
+            }
+            node = node->get(ch);
+        }
+        return node->isEnd();
+    }
+
+    // search if any word starts with this prefix
+    bool startsWith(string prefix) {
+        Node* node = root;
+        for (char ch : prefix) {
+            if (!node->containsKey(ch)) {
+                return false;
+            }
+            node = node->get(ch);
+        }
+        return true;
+    }
+};
+
+Trie trie;
